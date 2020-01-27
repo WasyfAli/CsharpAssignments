294b80063e7d97fd7f44e9fb4a83600cf77d2373
WasyfAli <46135061+WasyfAli@users.noreply.github.com>
2020-01-27 18:58:15 +0530

    Assignment One Uploaded Again

---
+++ AssignmentOne/AssignmentOne/Employee.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentOne
+{
+    class Employee
+    {
+        private int Id;
+        private string Name;
+        private string DepartmentName;
+        public delegate void Fired(String messg);
+        public event Fired onFired;
+
+        public Employee(int Id,string Name, string DepartmentName)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.DepartmentName = DepartmentName;
+
+        }
+
+        public int GetId()
+        {
+            onFired("GetId() method is called.");
+            return this.Id;
+        }
+        public string GetName()
+        {
+            onFired("GetName() method is called.");
+            return this.Name;
+        }
+        public string GetDEpartMentName()
+        {
+            onFired("GetDepartmentName() method is called.");
+            return this.DepartmentName;
+        }
+        //--------------------------------------------------
+    
+    }   
+
+}
+
---
+++ AssignmentOne/AssignmentOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AssignmentOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter employee ID: ");
+            int Id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name: ");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Enter DepartMent: ");
+            string DepartmentName = Console.ReadLine();
+            Employee emp = new Employee(Id, Name, DepartmentName);
+            emp.onFired += new Employee.Fired(call);
+            Console.WriteLine("======Printing Details=======");
+            Console.WriteLine("EMP Id: " +emp.GetId());
+            Console.WriteLine("EMP Name: " + emp.GetName());
+            Console.WriteLine("EMP DepartMent: " + emp.GetDEpartMentName());
+            Console.WriteLine("=============================");
+            static void call(string s)
+            {
+                Console.WriteLine($"{s}");
+            }
+
+        }
+    }
+}
