4a4e767b9609610bf8311db90a87f3cd13e1fd48
WasyfAli <46135061+WasyfAli@users.noreply.github.com>
2020-01-29 15:23:23 +0530

    Assignment 4 of C# uploaded

---
+++ Assignment_Four/Assignment_Four/Plurals.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment_Four
+{
+    public class Plurals
+    {
+        public void Plurals_of_words(string w)
+        {
+            int l = w.Length;
+            char[] alpha = w.ToCharArray();
+
+            if (alpha[l - 1] == 'y')
+            {
+
+                alpha[l - 1] = 'i';
+                alpha[l] = 'e';
+                alpha[l + 1] = 's';
+                alpha[l + 2] = '\0';
+            }
+            else if (alpha[l - 1] == 's' ||
+                   (alpha[l - 2] == 'c' && alpha[l - 1] == 'h') ||
+                   (alpha[l - 2] == 's' && alpha[l - 1] == 'h'))
+            {
+
+                w += "es";
+            }
+
+            else
+            {
+                w += "s";
+            }
+
+            Console.WriteLine(w);
+
+            return;
+        }
+    }
+}
---
+++ Assignment_Four/Assignment_Four/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Assignment_Four
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new List<string>();
+            words.Add("Boat");
+            words.Add("house");
+            words.Add("cat");
+            words.Add("river");
+            words.Add("cupboard");
+
+            Plurals p_obj = new Plurals();
+            foreach (var a in words)
+                p_obj.Plurals_of_words(a);
+
+            words[words.FindIndex(ind => ind.Equals("house"))] = "Home";
+
+            Console.WriteLine();
+            p_obj.Plurals_of_words(words[1]);
+
+            words.Add("cabaret");
+            words.Add("habitat");
+            words.Add("laptop");
+            words.Add("season");
+
+            Console.WriteLine();
+            Console.WriteLine(words.Count);
+            Console.WriteLine();
+            foreach (var b in words)
+            {
+                if (b.Length == 7)
+                    Console.WriteLine(b);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine(words[2]);
+            Console.WriteLine();
+
+            words.Sort();
+            foreach (var c in words)
+                Console.WriteLine(c);
+
+            Console.WriteLine();
+            words.Reverse();
+            foreach (var d in words)
+                Console.WriteLine(d);
+        }
+    }
+}
+// Boat, house, cat, river, cupboard
